24b98f641177a2feeaa706dc85c915d9112edcec
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2021-07-27 12:51:38 +0300

    hw-13

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './sass/styles.scss';
+
+import getRefs from './js/refs';
+
+import countryCardTpl from './templates/gallery-card.hbs';
+
+import fetchCountries from './js/fetchAPI';
+
+// import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    form: document.querySelector('.search-form'),
+    imgBox: document.querySelector('.gallery'),
+    loadBtn: document.querySelector('.load-more'),
+  };
+}
